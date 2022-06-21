c32114e78e74b221e2973e3e39ee8dd15c51f4dc
enzofac <efaciano@gmail.com>
2022-06-21 03:13:54 -0300

    Agregado de puntos 3, 4 y 5

--- examen2/scriptpractica.sql
+++ examen2/scriptpractica.sql
@@ -2,7 +2,7 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
 -- Schema Examen2021Kameyha
@@ -178,7 +178,7 @@ CREATE INDEX `fk_Pagos_Clientes1_idx` ON `Examen2021Kameyha`.`Pagos` (`idCliente
 CREATE INDEX `fk_Pagos_Alquileres1_idx` ON `Examen2021Kameyha`.`Pagos` (`idAlquiler` ASC) VISIBLE;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
+-- SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
@@ -191,4 +191,97 @@ caso de 2 clientes con la misma cantidad de alquileres, alfabéticamente según 
 nombre. Incluir el código con la consulta a la vista.*/
 
 DROP VIEW IF EXISTS VRankingAlquileres;
-CREATE VIEW VRankingAlquileres (Codigo,Nombre,Apellido)
+CREATE VIEW VRankingAlquileres (Codigo,Apellidos,Nombres,CantidadAlquileres) -- duda de si debería poner apellido y nombre en misma columna
+AS
+	SELECT Clientes.idCliente, Clientes.apellidos, Clientes.nombres, COUNT(Peliculas.idPelicula) AS CantidadAlquileres
+    FROM
+    Clientes INNER JOIN Alquileres ON Clientes.idCliente = Alquileres.idCliente
+			 INNER JOIN Registros ON Alquileres.idRegistro = Registros.idRegistro
+             INNER JOIN Peliculas ON Registros.idPelicula = Peliculas.idPelicula
+	GROUP BY Clientes.idCliente, Clientes.apellidos, Clientes.nombres
+    ORDER BY CantidadAlquileres DESC, Clientes.apellidos ASC, Clientes.nombres ASC
+    LIMIT 10;
+
+SELECT * FROM VRankingAlquileres;    
+
+/*3) Realizar un procedimiento almacenado llamado BorrarPelicula para borrar una película,
+incluyendo el control de errores lógicos y mensajes de error necesarios (implementar la
+lógica del manejo de errores empleando parámetros de salida). Incluir el código con la
+llamada al procedimiento probando todos los casos con datos incorrectos y uno con datos
+correctos.*/
+
+DROP PROCEDURE IF EXISTS spBorrarPelicula;
+DELIMITER //
+CREATE PROCEDURE spBorrarPelicula(IN pIdPelicula INT, OUT mensaje VARCHAR(100))
+BEGIN
+	IF pIdPelicula IS NULL THEN
+		SET mensaje = 'ERROR: Código de película vacío';
+	ELSEIF EXISTS (SELECT * FROM Registros WHERE Registros.idPelicula = pIdPelicula) THEN
+		SET mensaje = 'ERROR: La pelicula tiene asociado registro/s';
+	ELSEIF NOT EXISTS (SELECT * FROM Peliculas WHERE Peliculas.idPelicula = pIdPelicula) THEN 
+		SET mensaje = 'ERROR: La pelicula no existe';
+	ELSE
+		START TRANSACTION;
+			DELETE FROM Peliculas WHERE Peliculas.idPelicula = pIdPelicula;
+			SET mensaje = 'Pelicula borrada con éxito';
+	END IF;
+END //
+DELIMITER ;
+CALL spBorrarPelicula(null,@resultado); -- parámetro null
+SELECT @resultado;
+SELECT * FROM Registros;
+CALL spBorrarPelicula(1,@resultado); -- película asociada a registro
+SELECT @resultado;
+SELECT * FROM Peliculas;
+CALL spBorrarPelicula(2000,@resultado); -- película no existe
+SELECT @resultado;
+SELECT * FROM Peliculas LEFT JOIN Registros ON Peliculas.idPelicula = Registros.idPelicula;-- id 14 no tiene registros
+CALL spBorrarPelicula(14,@resultado); -- correcto
+SELECT @resultado;
+SELECT * FROM Peliculas;
+
+/*4) Realizar un procedimiento almacenado llamado TotalAlquileres que reciba el código de
+un cliente y muestre por cada película la cantidad de veces que la alquiló. Se deberá
+mostrar el código de la película, su título y la cantidad de veces que fue alquilada. Al final
+del listado deberá mostrar también la cantidad total de alquileres efectuados. La salida
+deberá estar ordenada alfabéticamente según el título de la película. Incluir en el código la
+llamada al procedimiento.*/
+
+DROP PROCEDURE IF EXISTS spTotalAlquileres;
+DELIMITER //
+CREATE PROCEDURE spTotalAlquileres (IN pIdCliente INT)
+BEGIN
+	SELECT Peliculas.idPelicula, Peliculas.titulo AS 'Título', COUNT(Peliculas.idPelicula) AS 'Cantidad'
+    FROM
+    Clientes INNER JOIN Alquileres ON Clientes.idCliente = Alquileres.idCliente
+			 INNER JOIN Registros ON Alquileres.idRegistro = Registros.idRegistro
+             INNER JOIN Peliculas ON Registros.idPelicula = Peliculas.idPelicula
+	WHERE Clientes.idCliente = pIdCliente
+	GROUP BY Peliculas.idPelicula  WITH ROLLUP
+    ORDER BY Peliculas.titulo ASC;
+END //
+DELIMITER ;
+-- se tuvo que modificar el SQL_MODE, sacando ONLY_FULL_GROUP_BY
+CALL spTotalAlquileres(1);-- queda un nombre en la columna titulo
+
+/*5) Utilizando triggers, implementar la lógica para que en caso que se quiera crear una
+película ya existente según código y/o título se informe mediante un mensaje de error que
+no se puede. Incluir el código con las creaciones de películas existentes según código y/o
+título y otro inexistente.*/
+
+DROP TRIGGER IF EXISTS `Trigger1_Creacion_Pelicula`; 
+DELIMITER //
+CREATE TRIGGER `Trigger1_Creacion_Pelicula` 
+BEFORE INSERT ON `Peliculas` FOR EACH ROW
+BEGIN
+	IF EXISTS (SELECT * FROM Peliculas WHERE Peliculas.idPelicula = NEW.idPelicula OR Peliculas.titulo = NEW.titulo) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERROR: Pelicula existente', MYSQL_ERRNO = 45000;
+	END IF;
+END //
+DELIMITER ;
+
+INSERT INTO `Peliculas` VALUES (1,'XDDDDDDDDDDDDDDDDDDDDDDDDDDD','PG',2006,86);-- id repetido
+SELECT * FROM Peliculas WHERE idPelicula=5000;-- no existe
+INSERT INTO `Peliculas` VALUES (5000,'ACADEMY DINOSAUR','PG',2006,86);-- titulo repetido
+INSERT INTO `Peliculas` VALUES (5000,'XDDDDDDDDDDDDDDDDDDDDDDDDDDD','PG',2006,86);-- correcto
+SELECT * FROM Peliculas WHERE idPelicula=5000;-- ahora existe
